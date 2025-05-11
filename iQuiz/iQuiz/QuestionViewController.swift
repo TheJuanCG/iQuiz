f990f9881b41bf43b040f52a4205ce1a2c4d3745
TheJuanCG <114948315+TheJuanCG@users.noreply.github.com>
2025-05-10 18:34:54 -0700

    Data controll for question view controller added

--- iQuiz/iQuiz/QuestionViewController.swift
+++ iQuiz/iQuiz/QuestionViewController.swift
@@ -9,12 +9,44 @@ import Foundation
 
 import UIKit
 
-class QuestionViewController: UIViewController {
+class QuestionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var sourceQuiz : Quiz? = nil
+    @IBOutlet weak var questionsTableView: UITableView!
+    
+    var selectedQuiz : Quiz?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Question"
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Selecting answer (AKA row)
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            print(cell.textLabel?.text ?? "No text")
+        }
+        
+        print("Selected the row")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let quiz = selectedQuiz {
+            return quiz.questions.count
+        }
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Trying to see if there is a previous available cell and using it for the new cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell", for: indexPath)
+        
+        cell.textLabel?.text = selectedQuiz?.questions[indexPath.row].text
+        return cell
     }
 }
